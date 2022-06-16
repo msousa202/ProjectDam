1e88dfe8f2561102df35b9e6e81f0a33ee16b3a0
ColherAlada <gpiteira10@gmail.com>
2022-06-16 23:21:58 +0100

    Começo de gráficos

--- app/src/main/java/com/example/project/GraphsActivity.java
+++ app/src/main/java/com/example/project/GraphsActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.ArrayList;
+
 public class GraphsActivity extends AppCompatActivity {
+
+    LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphs);
+
+        lineChart = (LineChart) findViewById(R.id.lineChart);
+
+        ArrayList<String> xAXES = new ArrayList<>();
+        ArrayList<Entry> yAXESsin = new ArrayList<>();
+        ArrayList<Entry> yAXEScos = new ArrayList<>();
     }
 }
