ac11b4911554ff331196f9047743fa54e9dc18fd
ColherAlada <gpiteira10@gmail.com>
2022-06-18 22:25:23 +0100

    historico

--- app/src/main/java/com/example/project/GraphsActivity.java
+++ app/src/main/java/com/example/project/GraphsActivity.java
@@ -15,15 +15,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.core.OrderBy;
+import com.google.firestore.v1.StructuredQuery;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class GraphsActivity extends AppCompatActivity {
 
     String UUID;
-    TextView age, height, weight;
     ArrayAdapter<String> adapter;
     ArrayList<String> arrayList;
     ListView list;
@@ -40,28 +43,24 @@ public class GraphsActivity extends AppCompatActivity {
 
         UUID = fAuth.getCurrentUser().getUid();
 
-        age = findViewById(R.id.textViewAge);
-        height = findViewById(R.id.textViewHeight);
-        weight = findViewById(R.id.textViewWeight);
-
         list = (ListView) findViewById(R.id.listViewHistory);
         arrayList = new ArrayList<String>();
 
         adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
         list.setAdapter(adapter);
 
-        Task<QuerySnapshot> documentReference = db.collection("userData").whereEqualTo("UUID", UUID).orderBy("date").get();
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        Task<QuerySnapshot> documentReference = db.collection("userData").whereEqualTo("UUID", UUID).orderBy("date", Query.Direction.ASCENDING).get();
         documentReference.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
                         Log.d(TAG, document.getId() + " => " + document.getData());
-                        age.setText(document.getString("age"));
-                        height.setText(document.getString("height"));
-                        weight.setText(document.getString("weight"));
-
-                        arrayList.add("Age: " + document.getString("age"));
+                        double imc =(Double.parseDouble(document.getString("weight")))/(Double.parseDouble(document.getString("height"))*Double.parseDouble(document.getString("height")));
+                        arrayList.add("IMC "+ df.format(imc) +" Age: " + document.getString("age")+" Height: " + document.getString("height")+" Weight: " + document.getString("weight") );
+                        arrayList.add("^ Date: " + document.getString("date"));
                         adapter.notifyDataSetChanged();
 
                     }
